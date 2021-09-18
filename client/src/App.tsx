902366ae941e0f0a2df254d69543fad19d6a81d3
Patryk Plaza <atrykp0@gmail.com>
2021-09-18 16:06:11 +0200

    add navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   const [data, setData] = useState<any>(null);
@@ -10,11 +10,9 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div className="app-wrapper">
+      <NavBar />
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <h1>navbar</h1>
+    </div>
+  );
+};
+
+export default NavBar;
