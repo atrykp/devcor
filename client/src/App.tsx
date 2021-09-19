05529ee4cfc1c5b736b3a41a3fe91d5521bc4264
Patryk Plaza <atrykp0@gmail.com>
2021-09-19 09:04:56 +0200

    add elements to main page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,26 @@ function App() {
   return (
     <div className="app-wrapper">
       <NavBar />
-      <p>{!data ? "Loading..." : data}</p>
+      <div className="header-wrapper">
+        <div className="header-content">
+          <p className="header-content-text">
+            Perfect place for developers who want improve skills all the time!
+          </p>
+          <div className="header-content-img"></div>
+        </div>
+      </div>
+      <div className="main-quote">
+        <p className="main-quote-text">
+          Become addicted to constant and never-ending self-improvement.
+        </p>
+        <p className="main-quote-author"> - Anthony J. D’Angelo</p>
+      </div>
+      <div className="main-information">
+        <p className="main-information-header">Functions</p>
+      </div>
+      <footer className="main-footer">
+        <p>{!data ? "Loading..." : data}</p>
+      </footer>
     </div>
   );
 }
