1b9c206100d030d8561322fd05646172277436a0
Patryk Plaza <atrykp0@gmail.com>
2021-09-23 07:02:44 +0200

    change form depend we are on login or sign up page

--- client/src/Screens/AuthorizationScreen/AuthorizationScreen.tsx
+++ client/src/Screens/AuthorizationScreen/AuthorizationScreen.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import Modal from "../../components/Modal/Modal";
@@ -15,15 +15,30 @@ interface IFormInput {
 }
 
 const AuthorizationScreen = () => {
-  const { type } = useParams<{ type: "signUp" | "login" }>();
+  const { type } = useParams<{ type: "signup" | "login" }>();
   const { register, handleSubmit } = useForm<IFormInput>();
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const isSignUp = type === "signup";
+  console.log(type);
+
+  const history = useHistory();
+
+  const changePage = () => {
+    history.push(
+      `${isSignUp ? "/authorization/login" : "/authorization/signup"}`
+    );
+  };
 
   return (
     <div className="auth-screen" onSubmit={handleSubmit(onSubmit)}>
-      <Modal title="Sign Up">
+      <Modal title={isSignUp ? "Sign Up" : "Login"}>
         <form action="" className="auth-screen__form">
-          <Input styles={"input__auth-screen input"} placeholderTxt={"name"} />
+          {isSignUp && (
+            <Input
+              styles={"input__auth-screen input"}
+              placeholderTxt={"name"}
+            />
+          )}
           <Input styles={"input__auth-screen input"} placeholderTxt={"email"} />
           <Input
             styles={"input__auth-screen input"}
@@ -33,10 +48,10 @@ const AuthorizationScreen = () => {
 
         <div className="auth-screen__buttons">
           <Button styles={"button button--accent"}>
-            {type !== "signUp" ? SIGN_UP : LOGIN}
+            {isSignUp ? SIGN_UP : LOGIN}
           </Button>
-          <Button styles={"button button--secondary"}>
-            {type === "signUp" ? SIGN_UP : LOGIN}
+          <Button styles={"button button--secondary"} callback={changePage}>
+            {!isSignUp ? SIGN_UP : LOGIN}
           </Button>
         </div>
       </Modal>
