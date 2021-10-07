b5b24ca2ec205e5437b964037c5226b5a4fd9183
Patryk Plaza <atrykp0@gmail.com>
2021-10-07 05:39:56 +0200

    add menu list item component

--- client/src/components/MenuList/MenuList.tsx
+++ client/src/components/MenuList/MenuList.tsx
@@ -1,13 +1,14 @@
+import MenuListItem from "../MenuListItem/MenuListItem";
 import "./MenuList.scss";
 
 const MenuList = () => {
   return (
     <div className="menu-list__wrapper">
       <ul className="menu-list__list">
-        <li>profile</li>
-        <li>english</li>
-        <li>notes</li>
-        <li>logout</li>
+        <MenuListItem />
+        <MenuListItem />
+        <MenuListItem />
+        <MenuListItem />
       </ul>
     </div>
   );
---
+++ client/src/components/MenuListItem/MenuListItem.tsx
@@ -0,0 +1,7 @@
+import "./MenuListItem.scss";
+
+const MenuListItem = () => {
+  return <h1>menuListItem</h1>;
+};
+
+export default MenuListItem;
