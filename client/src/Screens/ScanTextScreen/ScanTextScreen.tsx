6f4a1d6c3c201d3f0b3b2a16fa45605be4e53e65
Patryk Plaza <atrykp0@gmail.com>
2021-10-24 09:21:19 +0200

    add styles to scan text screen and add range input

--- client/src/Screens/ScanTextScreen/ScanTextScreen.tsx
+++ client/src/Screens/ScanTextScreen/ScanTextScreen.tsx
@@ -1,7 +1,11 @@
+import { toInteger } from "lodash";
+import { useRef, useState } from "react";
 import Button from "../../components/Button/Button";
 import "./ScanTextScreen.scss";
 
 const ScanTextScreen = () => {
+  const [rangeValue, setRangeValue] = useState(3);
+  const rangeRef = useRef<HTMLInputElement>(null!);
   return (
     <div className="scan-text-screen">
       <p className="scan-text-screen__title">Scan text</p>
@@ -12,16 +16,28 @@ const ScanTextScreen = () => {
       </div>
       <textarea className="scan-text-screen__text-area"></textarea>
 
-      <label htmlFor="ignoreList">
-        <input type="checkbox" name="ignore my words" id="ignoreList" />
-        Ignore words from my ignore list
-      </label>
+      <div className="scan-text-screen__check-boxes">
+        <label htmlFor="ignoreList">
+          <input type="checkbox" name="ignore my words" id="ignoreList" />
+          Ignore words from my ignore list
+        </label>
 
-      <label htmlFor="wordsList">
-        <input type="checkbox" name="ignore my words" id="wordsList" />
-        Ignore words from my list
-      </label>
-      <button>set min length</button>
+        <label htmlFor="wordsList">
+          <input type="checkbox" name="ignore my words" id="wordsList" />
+          Ignore words from my list
+        </label>
+      </div>
+      <div className="scan-text-screen__slider-input">
+        <input
+          type="range"
+          min="0"
+          max="20"
+          defaultValue={rangeValue}
+          onChange={() => setRangeValue(toInteger(rangeRef.current.value))}
+          ref={rangeRef}
+        />
+        <p>min. word length: {rangeValue}</p>
+      </div>
 
       <Button styles="scan-text-screen__scan-btn">Scan</Button>
     </div>
