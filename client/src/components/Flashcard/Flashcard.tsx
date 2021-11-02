7c32f1b15e5be6519f719e7fbf4b5b5c82679c2c
Patryk Plaza <atrykp0@gmail.com>
2021-11-02 06:17:55 +0100

    add edit mode to flashcards

--- client/src/components/Flashcard/Flashcard.tsx
+++ client/src/components/Flashcard/Flashcard.tsx
@@ -5,6 +5,7 @@ import { MenuButton } from "../Button/Button";
 import IconButton from "../IconButton/IconButton";
 import "./Flashcard.scss";
 import { useNotificationBar } from "../../hooks/useNotificationBar";
+import EditWordElement from "../EditWordElement/EditWordElement";
 
 type FlashcardsSite = "front" | "back";
 
@@ -31,6 +32,7 @@ const REMOVE_FLASHCARD = gql`
 
 const Flashcard = ({ data }: IFlashcard) => {
   const [currentSite, setCurrentSite] = useState<FlashcardsSite>("front");
+  const [isEdit, setIsEdit] = useState(false);
   const { from, to, fromLang, toLang, iCan, id } = data;
   const { showNotification } = useNotificationBar();
   const changeSite = () => {
@@ -54,39 +56,61 @@ const Flashcard = ({ data }: IFlashcard) => {
       showNotification("couldn't remove", "error");
     }
   };
+
+  const onEdit = (from: string, to: string) => {
+    console.log({ from, to });
+  };
   return (
     <div
       className={`flashcard ${currentSite === "back" ? "flashcard--back" : ""}`}
     >
-      <div className="flashcard__text-wrapper">
-        <p className="flashcard__text-language">
-          {currentSite === "front" ? fromLang : toLang}
-        </p>
-        <div
-          className={`flashcard__translation ${
-            currentSite === "back" ? "flashcard__translation--back" : ""
-          }`}
-        >
-          <p className="flashcard__text">
-            {currentSite === "front" ? from : to}
-          </p>
-        </div>
-      </div>
-      <div className="flashcard__buttons">
-        <div className="flashcard__can-button">
-          <MenuButton styles="flashcard__button">iCan</MenuButton>
-          {iCan && <i className="fas fa-check flashcard__can-symbol"></i>}
-        </div>
-        <IconButton
-          styles="button--round flashcard__button-turn-over"
-          callback={changeSite}
-        >
-          <i className="fas fa-sync-alt"></i>
-        </IconButton>
-        <MenuButton styles="flashcard__button" callback={removeWordElement}>
-          remove
-        </MenuButton>
-      </div>
+      {isEdit ? (
+        <EditWordElement
+          from={from}
+          to={to}
+          closeEdit={() => setIsEdit(false)}
+          onEditWord={onEdit}
+        />
+      ) : (
+        <>
+          {" "}
+          <div className="flashcard__text-wrapper">
+            <div className="flashcard__top-bar">
+              <i
+                className="fas fa-pencil-alt"
+                onClick={() => setIsEdit(true)}
+              ></i>
+              <p className="flashcard__text-language">
+                {currentSite === "front" ? fromLang : toLang}
+              </p>
+            </div>
+            <div
+              className={`flashcard__translation ${
+                currentSite === "back" ? "flashcard__translation--back" : ""
+              }`}
+            >
+              <p className="flashcard__text">
+                {currentSite === "front" ? from : to}
+              </p>
+            </div>
+          </div>
+          <div className="flashcard__buttons">
+            <div className="flashcard__can-button">
+              <MenuButton styles="flashcard__button">iCan</MenuButton>
+              {iCan && <i className="fas fa-check flashcard__can-symbol"></i>}
+            </div>
+            <IconButton
+              styles="button--round flashcard__button-turn-over"
+              callback={changeSite}
+            >
+              <i className="fas fa-sync-alt"></i>
+            </IconButton>
+            <MenuButton styles="flashcard__button" callback={removeWordElement}>
+              remove
+            </MenuButton>
+          </div>
+        </>
+      )}
     </div>
   );
 };
