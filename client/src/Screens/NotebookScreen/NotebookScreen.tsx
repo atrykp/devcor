67bc1d71c8ff2bd4afd8e10d5c0096706f06b004
Patryk Plaza <atrykp0@gmail.com>
2021-11-10 07:18:01 +0100

    add modal to create new note

--- client/src/Screens/NotebookScreen/NotebookScreen.tsx
+++ client/src/Screens/NotebookScreen/NotebookScreen.tsx
@@ -1,11 +1,15 @@
 import { useMutation, gql } from "@apollo/client";
 import { useContext, useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { useParams } from "react-router";
 import IconButton from "../../components/IconButton/IconButton";
+import Input from "../../components/Input/Input";
+import Modal from "../../components/Modal/Modal";
 import NoteElement from "../../components/NoteElement/NoteElement";
 import Title from "../../components/Title/Title";
 import { NotebookCtx } from "../../context/NotebookContext";
 import { useAuth } from "../../hooks/useAuth";
+import { useNotificationBar } from "../../hooks/useNotificationBar";
 import "./NotebookScreen.scss";
 
 const ADD_NOTE = gql`
@@ -17,14 +21,44 @@ const ADD_NOTE = gql`
   }
 `;
 
+type NoteInputs = {
+  title: string;
+  text: string;
+};
+
 const NotebookScreen = () => {
   useAuth("protect");
+  const [isAddNote, setIsAddNote] = useState(false);
   const [notebook, setNotebook] = useState<any>(null);
   const { id } = useParams<{ id: string }>();
   const noteCtx = useContext(NotebookCtx);
   const [addNote] = useMutation(ADD_NOTE, {
     refetchQueries: ["GetNotebookObj"],
   });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<NoteInputs>();
+
+  const { showNotification } = useNotificationBar();
+
+  const onSubmit: SubmitHandler<NoteInputs> = async (data) => {
+    showNotification("Adding note", "pending");
+    const { data: saveResult } = await addNote({
+      variables: {
+        title: data.title,
+        text: data.text,
+        notebookId: id,
+      },
+    });
+    if (!saveResult.addNote.status)
+      return showNotification(saveResult.addNote.message, "error");
+    showNotification(saveResult.addNote.message, "done");
+    reset();
+    setIsAddNote(false);
+  };
   useEffect(() => {
     const [notebook] = noteCtx.notebooks.filter(
       (element: any) => element.id === id
@@ -34,6 +68,33 @@ const NotebookScreen = () => {
 
   return (
     <div>
+      {isAddNote && (
+        <Modal
+          title="Add note"
+          confirmTxt={"save"}
+          cancelTxt={"cancel"}
+          cancelCallback={() => {
+            reset();
+            setIsAddNote(false);
+          }}
+          confirmCallback={() => handleSubmit(onSubmit)()}
+        >
+          <div className="modal__inputs">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                styles={errors?.title ? "input--error" : ""}
+                placeholder="title"
+                {...register("title", { required: true })}
+              />
+              <Input
+                styles={errors?.text ? "input--error" : ""}
+                placeholder="text"
+                {...register("text", { required: true })}
+              />
+            </form>
+          </div>
+        </Modal>
+      )}
       <Title text={notebook?.name} isBackButton />
       <p className="notebook-screen__info">Notes:</p>
       <div className="notebook-screen__notes">
@@ -50,7 +111,7 @@ const NotebookScreen = () => {
         )}
       </div>
       <IconButton
-        callback={() => console.log("add note")}
+        callback={() => setIsAddNote(true)}
         styles="button--round button--add"
       >
         <i className="fas fa-plus"></i>
