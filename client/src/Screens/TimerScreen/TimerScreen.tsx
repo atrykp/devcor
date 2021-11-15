6bacfadbf6220352d6fd66fb87129778778fbbcd
Patryk Plaza <atrykp0@gmail.com>
2021-11-15 06:12:54 +0100

    add style to the timer screen

--- client/src/Screens/TimerScreen/TimerScreen.tsx
+++ client/src/Screens/TimerScreen/TimerScreen.tsx
@@ -7,13 +7,20 @@ import "./TimerScreen.scss";
 const TimerScreen = () => {
   useAuth("protect");
   const [rangeValue, setRangeValue] = useState(25);
+  const [second, setSecond] = useState(0);
 
   const rangeRef = useRef<HTMLInputElement>(null!);
   return (
     <div>
       <Title text="Timer" isBackButton />
+      <div className="timer-screen__state-wrapper">
+        <p className="timer-screen__state">break</p>
+        <p className="timer-screen__state timer-screen__state--active">focus</p>
+      </div>
       <div className="timer-screen__top-bar"></div>
-      <p className="timer-screen__timer">{rangeValue}</p>
+      <p className="timer-screen__timer">
+        {rangeValue}:{second}
+      </p>
       <input
         type="range"
         min="0"
@@ -21,10 +28,13 @@ const TimerScreen = () => {
         defaultValue={rangeValue}
         onChange={() => setRangeValue(toInteger(rangeRef.current.value))}
         ref={rangeRef}
+        className="timer-screen__range-input"
       />
-      <i className="fas fa-play"></i>
-      <i className="fas fa-pause"></i>
-      <i className="fas fa-stop"></i>
+      <div className="timer-screen__control">
+        <i className="fas fa-play"></i>
+        <i className="fas fa-pause"></i>
+        <i className="fas fa-stop"></i>
+      </div>
     </div>
   );
 };
