c6da9aff258ef4df42157afd71572a39a9be4edf
Patryk Plaza <atrykp0@gmail.com>
2021-11-19 05:56:27 +0100

    add TimersHistory page

---
+++ client/src/Screens/TimersHistory/TimersHistory.tsx
@@ -0,0 +1,12 @@
+import Title from "../../components/Title/Title";
+import "./TimersHistory.scss";
+
+const TimersHistory = () => {
+  return (
+    <div>
+      <Title text="Timers" />
+    </div>
+  );
+};
+
+export default TimersHistory;
